d9879fd89146bfcc106b04ccc7b3d92bf018f862
DrMetr <dashagamlo@mail.ru>
2025-03-22 19:52:06 +0300

    finishing

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const button = document.querySelector("button");
+const submit_button = document.querySelector("button");
 const fnameInput = document.querySelector("#fname");
 const lnameInput = document.querySelector("#lname");
 const emailInput = document.querySelector("#email");
 const messageInput = document.querySelector("#message");
 const consentInput = document.querySelector("#consent");
 const queryTypeInput = document.querySelectorAll("input[type=radio]");
+const toast = document.querySelector(".toast");
+const close_toast_button = document.querySelector("#close_toast");
 
 const emailRegExp = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 
@@ -12,7 +14,7 @@ function validateEmail(emailInput){
     return emailRegExp.test(String(emailInput).toLowerCase());
   }
 
-button.addEventListener("click", (event) => {
+submit_button.addEventListener("click", (event) => {
     event.preventDefault();
 
     if (!validateEmail(emailInput.value)) {
@@ -21,6 +23,12 @@ button.addEventListener("click", (event) => {
     } else {
         emailInput.classList.remove("error_state");
         emailInput.nextElementSibling.style.display = "none";
+    }
+
+    if (!(queryTypeInput[0].checked || queryTypeInput[1].checked)) {
+        document.querySelector("#queryType_error_message").style.display = "inline";
+    } else {
+        document.querySelector("#queryType_error_message").style.display = "none";
     }
 
     if (!consentInput.checked) {
@@ -40,4 +48,21 @@ button.addEventListener("click", (event) => {
         }
     });
 
+    let errors = document.querySelectorAll(".error_message");
+    for (let error of errors){
+        if (error.style.display != "none"){
+            return;
+        } else {
+            if (toast.classList.contains("hide")) {
+            toast.classList.replace("hide", "show");
+            } else {
+                toast.classList.add("show");
+            }}}
+    
+
+})
+
+close_toast_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    toast.classList.replace("show", "hide");
 })
